001bf63e252fc16f68e338f9b054e3bd1ded9bb0
deepak14ri <deepakchaudhari705@gmail.com>
2024-04-29 22:45:09 +0530

    longest common prefixes

---
+++ 14. Longest Common Prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let pref = strs[0];
+    let prefLen = pref.length;
+    for(let i=1;i<strs.length;i++){
+        let s = strs[i];
+        while(pref !== s.substring(0, prefLen)){
+            prefLen--;
+            if(prefLen === 0){
+                return '';
+            }
+            pref = pref.substring(0, prefLen);
+        }
+    }
+    return pref;
+};
