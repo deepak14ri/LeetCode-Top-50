c9eb406d6f5a1edabc085b83b566495df5956b0f
deepak14ri <deepakchaudhari705@gmail.com>
2024-05-12 00:05:27 +0530

    add binary

---
+++ 10. Regular Expression Matching.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+       
+    const dp = Array(s.length + 1).fill(false).map(() => Array(p.length + 1).fill(false));
+
+    dp[0][0] = true;
+
+    for (let j = 1; j <= p.length; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 2];
+        }
+    }
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = 1; j <= p.length; j++) {
+            if (p[j - 1] === s[i - 1] || p[j - 1] === '.') {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] === '*') {
+                dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] === p[j - 2] || p[j - 2] === '.'));
+            }
+        }
+    }
+
+    return dp[s.length][p.length];
+};
+
+console.log(isMatch(s = "aa", p = "a*"));
