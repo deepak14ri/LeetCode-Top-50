f1aa269f98263878b4b65dbcb39d9161b169493f
deepak14ri <deepakchaudhari705@gmail.com>
2024-05-23 21:36:32 +0530

    test

---
+++ 49. Group Anagrams.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const mp = new Map();
+    const ans = [];
+
+    for(let str of strs){
+        const sortedStr = str.split('').sort().join('');
+        if(mp.has(sortedStr)){
+            ans[mp.get(sortedStr)].push(str);
+        }else{
+            mp.set(sortedStr, ans.length);
+            ans.push([str]);
+        }
+    }
+    return ans;
+};
