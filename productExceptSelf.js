c57d60111c8e3c28f58a4c2355e56e3c5be6393e
deepak14ri <deepakchaudhari705@gmail.com>
2024-06-30 01:28:39 +0530

    product except self

---
+++ productExceptSelf.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let length = nums.length;
+    let ans = new Array(length).fill(1);
+
+    let leftProduct = 1;
+    for (let i = 0; i < length; i++) {
+        ans[i] *= leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    let rightProduct = 1;
+    for (let i = length - 1; i >= 0; i--) {
+        ans[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return ans;
+};
+
+// Example usage:
+console.log(productExceptSelf([1, 2, 3, 4])); // Output: [24, 12, 8, 6]
+console.log(productExceptSelf([0, 0]));       // Output: [0, 0]
+console.log(productExceptSelf([0, 4, 5]));    // Output: [20, 0, 0]
